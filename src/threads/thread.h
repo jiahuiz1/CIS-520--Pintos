7583526c210d17b64b29d498383e0729a8564b5f
jiahuiz1 <46904850+jiahuiz1@users.noreply.github.com>
2021-02-20 02:24:53 -0600

    Update thread.h

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -129,6 +129,8 @@ const char *thread_name (void);
 void thread_exit (void) NO_RETURN;
 void thread_yield (void);
 
+bool cmp_priority(const struct list_elem *, const struct list_elem *, void *); /*modified*/
+  
 /* Performs some operation on thread t, given auxiliary data AUX. */
 typedef void thread_action_func (struct thread *t, void *aux);
 void thread_foreach (thread_action_func *, void *);
