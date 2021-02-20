3539df5437b5c6fa47668e9fc44da9f9113fb3e0
jiahuiz1 <46904850+jiahuiz1@users.noreply.github.com>
2021-02-20 01:13:06 -0600

    Update thread.h

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -122,8 +122,6 @@ tid_t thread_create (const char *name, int priority, thread_func *, void *);
 void thread_block (void);
 void thread_unblock (struct thread *);
 
-void thread_sleep (int64_t); /*modified*/
-
 struct thread *thread_current (void);
 tid_t thread_tid (void);
 const char *thread_name (void);
